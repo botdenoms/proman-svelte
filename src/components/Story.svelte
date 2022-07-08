686b39d5236ad09353c9312944823ab0753c0d86
botdenoms <dudubot404@gmail.com>
2022-07-08 14:04:16 +0300

    story component added

---
+++ src/components/Story.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let user = ''
+    export let story = ''
+</script>
+
+<div class="item">
+    <span>{user}</span>
+    <div class="content">
+        {story}
+    </div>
+</div>
+
+<style>
+    .item{
+        margin-bottom: 5px;
+    }
+
+    .item span{
+        color: #1e1e1e;
+    }
+
+    .content{
+        color: #288bb2;
+        border-bottom: 1px solid #474747;
+    }
+</style>
